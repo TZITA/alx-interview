399bcac6d474d986f52e696becdec9f99eeda4d3
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-06-28 15:45:22 +0300

    Initial commit + pascal triangle py code

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Pascal's Triangle """
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing
+    the Pascals triangle of n (n = number of rows).
+    """
+    array = []
+
+    for i in range(n):
+        arrayNew = []
+        for j in range(i + 1):
+            if j < 2:
+                arrayNew.append(1)
+            else:
+                arrayNew.insert((j - 1), (array[i - 1][j - 2] + array[i - 1][j - 1]))
+
+        array.append(arrayNew)
+    
+    return array
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,3 @@
+# ALX INTERVIEW
+## Pascals Triangle
+A python function that returns a list of lists representing the numbers in pascals triangle.
