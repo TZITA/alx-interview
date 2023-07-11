a5bff05ca7aa3e9bffa4040699e239d8cd5b4ab1
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-11 18:46:12 +0300

    MinOp fully functional

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,5 +4,11 @@
 
 def minOperations(n: int) -> int:
     """Calculates the minimum number of operations"""
-    if n < 6 or n % 2 != 0:
+    if type(n) != int:
+        return 0
+    elif n < 6 or n % 2 != 0:
         return n
+    else:
+        if n % 4 != 0:
+            return int(n / 2 + 2)
+        return int(n / 4 + 4)
