14e0100bd3c21bd3f40895cf3892bda868c2c0da
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-11 21:46:20 +0300

    Handled errors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 """ Minimum Operations function """
+import math
 
 
-def minOperations(n: int) -> int:
+def minOperations(n):
     """Calculates the minimum number of operations"""
+    prime_factors = []
+    minOp = 0
+
     if type(n) != int:
         return 0
-    elif n < 6 or n % 2 != 0:
-        return n
-    else:
-        if n % 4 != 0:
-            return int(n / 2 + 2)
-        return int(n / 4 + 4)
+
+    while n % 2 == 0:
+        prime_factors.append(2)
+        n /= 2
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            prime_factors.append(i)
+            n /= i
+
+    if n > 2:
+        prime_factors.append(n)
+
+    for i in range(len(prime_factors)):
+        minOp += prime_factors[i]
+
+    return int(minOp)
