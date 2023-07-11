3af42754b7aa352b08a281a6cb978b76d2c9618b
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-11 17:42:11 +0300

    Try 1

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" Minimum Operations function """
 
 
 def minOperations(n: int) -> int:
+    """Calculates the minimum number of operations"""
     if n < 6 or n % 2 != 0:
         return n
