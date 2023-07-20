c0ebd06066edb4c215ed4186f3db7689b05e926f
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-07-20 10:19:49 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,7 +12,7 @@ l_404 = 0
 l_405 = 0
 l_500 = 0
 
-for line in sys.stdin:
+for i, line in enumerate(sys.stdin, 1):
     log_parts = line.split()
 
     status_c = int(log_parts[7])
@@ -35,8 +35,7 @@ for line in sys.stdin:
     if status_c == 500:
         l_500 += 1
 
-    if (l_200 + l_301 + l_400 + l_401 +
-            l_403 + l_404 + l_405 + l_500) % 10 == 0:
+    if i % 10 == 0:
         print(f"File size: {file_size}")
         if l_200:
             print(f"200: {l_200}")
@@ -54,3 +53,21 @@ for line in sys.stdin:
             print(f"405: {l_405}")
         if l_500:
             print(f"500: {l_500}")
+
+print(f"File size: {file_size}")
+if l_200:
+    print(f"200: {l_200}")
+if l_301:
+    print(f"301: {l_301}")
+if l_400:
+    print(f"400: {l_400}")
+if l_401:
+    print(f"401: {l_401}")
+if l_403:
+    print(f"403: {l_403}")
+if l_404:
+    print(f"404: {l_404}")
+if l_405:
+    print(f"405: {l_405}")
+if l_500:
+    print(f"500: {l_500}")
