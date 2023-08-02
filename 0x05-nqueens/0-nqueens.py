df86c2f160797b000ae486d5a45fb4a83895c8f5
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-08-02 19:19:35 +0300

    Nqueens logic

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,20 +3,47 @@
 import sys
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
+def is_safe(board, row, col, N):
+    for i in range(row):
+        if board[i] == col or abs(board[i] - col) == abs(i - row):
+            return False
+    return True
 
-try:
-    N = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    exit(1)
 
-# if N is < 4, impossible to have non-attacking positions of the queens
-if N < 4:
-    print("N must be at least 4")
-    exit(1)
+def solve_n_queens(board, row, N, solutions):
+    if row == N:
+        solutions.append([[i, board[i]] for i in range(N)])
+        return
 
-else:
-    print(sys.argv[1])
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row] = col
+            solve_n_queens(board, row + 1, N, solutions)
+
+
+def n_queens(N):
+    board = [-1] * N
+    solutions = []
+    solve_n_queens(board, 0, N, solutions)
+    return solutions
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    # if N is < 4, impossible to have non-attacking positions of the queens
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = n_queens(N)
+    for solution in solutions:
+        print(solution)
