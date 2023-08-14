e8c41ebfcda23f61ac961f5414ee8b197745f6c3
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-08-14 16:13:48 +0300

    Init commit

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Rotate 2D matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """ Given an n x n 2D matrix,
+        this method rotates it 90 degrees clockwise.
+        Return: 
+           None
+    """
+
