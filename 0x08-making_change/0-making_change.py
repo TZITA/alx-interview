905d3464e26df3a982d86a3ace42988b2937e2b2
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-08-22 10:17:26 +0300

    failed checks try 1

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,12 +7,16 @@ def makeChange(coins, total):
     Returns the fewest number of coins needed
     to meet a given amount total.
     """
+    num_coins = 0
+
     if total <= 0:
         return 0
 
-    sorted_coins = sorted(coins)
-    coins = len(sorted_coins)
-    num_coins = 0
+    try:
+        sorted_coins = sorted(coins)
+        coins = len(sorted_coins)
+    except Exception:
+        return -1
 
     for i in range(1, coins):
         num_each_coin = int(total / sorted_coins[coins - i])
