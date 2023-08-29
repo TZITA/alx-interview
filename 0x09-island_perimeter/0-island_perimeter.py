327d5cd6144b310ce76611427c76ffcbf2a0e552
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-08-29 16:11:10 +0300

    Alternative approach

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,16 +3,14 @@
 
 
 def island_perimeter(grid):
-    """Returns perimeter of the island grid"""
+    """Returns perimeter of the island grid
+    """
+    perimeter = 0
 
-    sides_2 = 0
-    sides_3 = 0
-    sides_4 = 0
-
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            sides = 0
+    for i in range(1, len(grid) - 1):
+        for j in range(1, len(grid[i]) - 1):
             if grid[i][j] == 1:
+                sides = 0
                 # check the surrounding
                 if grid[i + 1][j] == 0:
                     sides += 1
@@ -23,15 +21,6 @@ def island_perimeter(grid):
                 if grid[i][j - 1] == 0:
                     sides += 1
 
-                if sides == 4:
-                    sides_4 += 1
-                # one square in contact with only one square
-                # contributes 3 units in the perimeter
-                elif sides == 3:
-                    sides_3 += 1
-                # one square in contact with two squares
-                # contributes 2 units in the perimeter
-                elif sides == 2:
-                    sides_2 += 1
+                perimeter += sides
 
-    return ((sides_4 * 4) + (sides_3 * 3) + (sides_2 * 2))
+    return perimeter
