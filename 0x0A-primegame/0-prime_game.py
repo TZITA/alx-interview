eebe4e44796276644e3f6617b4a3d8062666e5f6
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-09-05 10:34:17 +0300

    Handled edge cases

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -12,25 +12,33 @@ def isWinner(x, nums):
         2. None
     """
     winner = []
+
+    if nums == []:
+        return "Ben"
+
     for n in range(x):
         prime_factors = []
-        for i in range(1, nums[n]+1):
-            while i % 2 == 0:
-                prime_factors.append(2)
-                i /= 2
 
-            for j in range(3, int(i ** 0.5) + 1, 2):
-                while i % j == 0:
-                    prime_factors.append(j)
-                    i /= j
+        if nums[n] > 0:
+            for i in range(1, nums[n]+1):
+                while i % 2 == 0:
+                    prime_factors.append(2)
+                    i /= 2
 
-            if i > 2:
-                prime_factors.append(i)
+                for j in range(3, int(i ** 0.5) + 1, 2):
+                    while i % j == 0:
+                        prime_factors.append(j)
+                        i /= j
 
-        if len(prime_factors) % 2 == 0:
-            winner.append("Ben")
+                if i > 2:
+                    prime_factors.append(i)
+
+            if len(prime_factors) % 2 == 0:
+                winner.append("Ben")
+            else:
+                winner.append("Maria")
         else:
-            winner.append("Maria")
+            winner.append(None)
 
     if winner.count("Ben") > winner.count("Maria"):
         return "Ben"
